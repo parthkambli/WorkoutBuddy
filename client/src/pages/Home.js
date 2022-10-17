9f47add6919772921a335b2010fa257f202b2701
Parth <parthkambli.pk1@gmail.com>
2022-10-17 10:12:25 +0530

    Context and global state with custome hook(fixed the bug- reload all workouts after adding new one)

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
 
 import WorkoutDetails from '../components/WorkoutDetails';
 import WorkoutForm from '../components/WorkoutForm';
 
 const Home = () => {
-  const [workouts, setWorkouts] = useState(null);
+  const { workouts, dispatch } = useWorkoutsContext();
 
   useEffect(() => {
     const fetchWorkout = async () => {
@@ -12,11 +13,11 @@ const Home = () => {
       const json = await response.json();
 
       if (response.ok) {
-        setWorkouts(json);
+        dispatch({ type: 'SET_WORKOUTS', payload: json });
       }
     };
     fetchWorkout();
-  }, []);
+  }, [dispatch]);
 
   return (
     <div className="home">
