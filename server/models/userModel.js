45da335de30030ca483f9b6aa01b75bb8c945f14
Parth <parthkambli.pk1@gmail.com>
2022-10-24 10:48:50 +0530

    Email & Password Validation

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
+import validator from 'validator';
 
 const Schema = mongoose.Schema;
 
@@ -17,6 +18,17 @@ const userSchema = new Schema({
 
 // static signup method
 userSchema.statics.signup = async function (email, password) {
+  // Validation
+  if (!email || !password) {
+    throw Error('All fields must be filled!');
+  }
+  if (!validator.isEmail(email)) {
+    throw Error('email is not valid!');
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error('password not strong enough');
+  }
+
   const exist = await this.findOne({ email });
 
   if (exist) {
