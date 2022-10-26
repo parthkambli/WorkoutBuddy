c16e1350e1b28864fc930850d17d38bb42dbe962
Parth <parthkambli.pk1@gmail.com>
2022-10-26 10:02:15 +0530

    Login & Signup Form

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Navbar />
+        <Navbar />
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
           </Routes>
         </div>
       </BrowserRouter>
