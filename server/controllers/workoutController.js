dd1eb917068d576cb3e70c68d60e7a6e0e24523f
Parth <parthkambli.pk1@gmail.com>
2022-11-04 12:20:11 +0530

    Assigning Workouts to users and finishing the project

--- server/controllers/workoutController.js
+++ server/controllers/workoutController.js
@@ -3,7 +3,9 @@ import mongoose from 'mongoose';
 
 // Get all workout
 const getWorkouts = async (req, res) => {
-  const workouts = await Workout.find({}).sort({ createdAt: -1 });
+  const user_id = req.user._id
+
+  const workouts = await Workout.find({user_id}).sort({ createdAt: -1 });
   res.status(200).json(workouts);
 };
 
@@ -41,9 +43,10 @@ const createWorkout = async (req, res) => {
       .json({ eror: 'please fill in all the fields', emptyFields });
   }
 
-  // add doc to DB
+  // add doc to DB 
   try {
-    const workout = await Workout.create({ title, reps, load });
+    const user_id = req.user._id;
+    const workout = await Workout.create({ title, reps, load, user_id });
     res.status(200).json(workout);
   } catch (error) {
     res.status(400).json({ error: error.message });
